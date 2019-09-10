d7bba109c3d302f6754e172c35770a6362cfab10
Aliaksandr <a.daniuk@yahoo.com>
2019-09-10 12:17:09 +0300

    change login.views for  signup view

--- encrypted/login/views.py
+++ encrypted/login/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login, logout
+from django.urls import reverse_lazy
+from django.views import generic
+from . import forms
 # Create your views here.
+
+#User Signup view
+class SignUp(generic.CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy("login")
+    template_name = "login/signup.html"
