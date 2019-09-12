89b393392b24ca8810cbf3112bc10a33b6b93d8b
Aliaksandr <a.daniuk@yahoo.com>
2019-09-12 11:22:46 +0300

    fix staticfiles mestake insettings.py

--- encrypted/encrypted/settings.py
+++ encrypted/encrypted/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
-    'login'
+    'login',
+    'categories',
+    
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      
 ]
 
 ROOT_URLCONF = 'encrypted.urls'
@@ -120,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 
 
